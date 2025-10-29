1f445f337f0b664892704d1bfa564331c40c0a44
Min-su-Jeong <minsu1255@naver.com>
2025-10-30 08:01:39 +0900

    feat: 생성/수정일 자동 관리를 위한 JPA Auditing 활성화

--- src/main/java/com/kakaotechbootcamp/community/entity/PostImage.java
+++ src/main/java/com/kakaotechbootcamp/community/entity/PostImage.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @Table(name = "post_image")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class PostImage {
 
     @Id
